250a2b75c339bf651218a61b16ecf8f3f048ffba
Dloyr <d.loyer59870@gmail.com>
2024-03-05 15:01:30 +0100

    implementation of the method save of storage,the instance of  the class FileStorage, in save method. And implementation of the method new of storage in the constructor

--- models/base_model.py
+++ models/base_model.py
@@ -29,6 +29,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            from models import storage
+            storage.new(self)
 
     def __str__(self):
         """Return the string representation of object instance."""
@@ -38,6 +40,8 @@ class BaseModel:
     def save(self):
         """Updates the attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        from models import storage
+        storage.save()
 
     def to_dict(self):
         """return a dictionary containing all keys/values of class instances"""
