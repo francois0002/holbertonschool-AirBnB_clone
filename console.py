8a6b582796521786f8762b22768f6efe4aa307d9
Dloyr <d.loyer59870@gmail.com>
2024-03-06 11:25:23 +0100

    fix pycodestyle

--- console.py
+++ console.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-import cmd, sys
+import cmd
+import sys
 
 
 class HBNBCommand(cmd.Cmd):
-	""" HBNH console """
+    """ HBNH console """
     prompt = '(hbnb) '
 
-	def do_quit(self, line):
-		"""Quit command to exit the program"""
-		return True
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
 
-	def do_EOF(self, line):
-		"""Exit console"""
-		return True
+    def do_EOF(self, line):
+        """Exit console"""
+        return True
 
-	def emptyline(self):
-		pass
+    def emptyline(self):
+        pass
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
